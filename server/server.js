7d6bb12592aeb72116c07a5b642da0d2afe46ec8
Anand Singh <sanand926@gmail.com>
2018-10-12 16:45:45 +0530

    Add DELETE /todos/:id in TodoApp

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var { Todo } = require('./models/todo')
 var { User } = require('./models/user')
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -48,9 +49,26 @@ app.get('/todos/:id', (req, res) => {
     })
 })
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
 
-app.listen(3000, () => {
-    console.log('Started on port 3000')
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findOneAndDelete(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`)
 });
 
 module.exports = {app};
