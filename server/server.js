885c2adc97d636f2df5b22350d9e5e6d669f8ae1
Anand Singh <sanand926@gmail.com>
2018-10-15 19:37:54 +0530

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -129,7 +129,7 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user)
 });
 
-//POSt logging /users/login
+//POST logging /users/login
 
 app.post('/users/login', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
@@ -142,6 +142,17 @@ app.post('/users/login', (req, res) => {
         res.status(400).send(e);
     })
 })
+
+//DELETE logout /users/me/token
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send()
+    })
+})
+
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`)
